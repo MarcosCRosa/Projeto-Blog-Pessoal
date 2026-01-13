776bae3ccf3830685fd7326577a15970bf85f364
MarcosCRosa <marcosluiz4012@gmail.com>
2026-01-13 11:38:38 -0300

    first commit

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,31 @@
+function Home() {
+    return (
+        <>
+        <main>
+            <section style={{
+              display:"grid",
+              gridTemplateColumns:"1fr 1fr",
+              backgroundColor:"#5f46c4"
+            }}>
+                <article style={{
+                   display:"flex",
+                   flexDirection:"column",
+                   alignItems:"center",
+                   justifyContent:"center",
+                   color:"white"
+                }}>
+                 <h2 style={{fontSize:"3rem",fontWeight:"bold"}}>
+                  Seja Bem Vinde!</h2>
+                 <p style={{fontSize:"1.25rem"}}>Expresse aqui os seus pensamentos e opniões</p>
+                 <button style={{border:"1rem",borderRadius:"5px", padding:"0.5rem"}}>Nova Postagem</button>
+                </article>
+                <figure>
+                <img src="https://i.imgur.com/fyfri1v.png" alt="" style={{width:"100%"}}/>
+                </figure>
+            </section>
+        </main>
+        </>
+    )
+}
+
+export default Home
